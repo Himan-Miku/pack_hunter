6e8d77767ba9c1c2802f878823bef241425eb770
Himan-Miku <himanshu28.mj@gmail.com>
2023-05-20 19:03:34 +0530

    feat: add search_pack fn

--- src/javascript.rs
+++ src/javascript.rs
@@ -1,4 +1,20 @@
 use clap::Args;
 
-#[derive(Args)]
-pub struct JsOptions {}
+#[derive(Args, Debug)]
+pub struct JsOptions {
+    ///package name to search for
+    #[arg(short = 'q', long = "query")]
+    query: Option<String>,
+    ///number of results to display
+    #[arg(short = 'n', long = "num_results", default_value_t = 5)]
+    num_res: u8,
+}
+
+pub fn search_pack(JsOptions { query, num_res }: &JsOptions) {
+    match query {
+        Some(s) => {
+            println!("Hello query")
+        }
+        None => println!("None"),
+    }
+}
